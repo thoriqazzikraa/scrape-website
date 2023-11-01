6ba78427f6bcc60f6792622ab790ea16c156101f
thoriqazzikraa <thoriqazzikra17@gmail.com>
2023-11-02 01:06:49 +0700

    fixed pinterest downloader

--- src/downloader.js
+++ src/downloader.js
@@ -198,9 +198,14 @@ async function pindl(url) {
   try {
     const { data } = await axios.get(`https://www.savepin.app/download.php?url=${url}&lang=en&type=redirect`)
     const $ = cheerio.load(data)
+    if ($(".download-link > div:nth-child(2) > div > table > tbody >  tr:nth-child(1) > td:nth-child(3) > a").attr("href").includes("force-save.php")) {
+      var urll = decodeURIComponent($(".download-link > div:nth-child(2) > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > a").attr("href").split("url=")[1])
+    } else {
+      var urll = $(".download-link > div:nth-child(2) > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > a").attr("href")
+    }
     const result = {
       status: true,
-      url: decodeURIComponent($(".download-link > div:nth-child(2) > div > table > tbody >  tr:nth-child(1) > td:nth-child(3) > a").attr("href").split("url=")[1])
+      url: urll
     }
     console.log(result)
     return result
